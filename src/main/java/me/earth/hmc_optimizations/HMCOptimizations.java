3b2825fd85f7bc1e0a9076c241abb9a83fa3f20f
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2024-03-04 14:31:54 +0100

    [0.1.0] Parse Optimizations from SystemProperty

--- src/main/java/me/earth/hmc_optimizations/HMCOptimizations.java
+++ src/main/java/me/earth/hmc_optimizations/HMCOptimizations.java
@@ -3,7 +3,7 @@ package me.earth.hmc_optimizations;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public class HMCOptimizations {
-    public static boolean optimize = false; // Boolean.parseBoolean(System.getProperty("hmc.optimize", "true"));
+    public static boolean optimize = Boolean.parseBoolean(System.getProperty("hmc.optimize", "true"));
 
     public static void optimize(CallbackInfo ci) {
         if (optimize) {
