b0b9cf089a7cb56a0f24778b5547764f48cd7582
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2024-03-05 23:24:00 +0100

    Fixed NeoForgeMod

--- src/neoforge/java/me/earth/hmc_optimizations/neoforge/NeoForgeMod.java
+++ src/neoforge/java/me/earth/hmc_optimizations/neoforge/NeoForgeMod.java
@@ -1,19 +1,8 @@
 package me.earth.hmc_optimizations.neoforge;
 
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod("hmc_optimizations")
 public class NeoForgeMod {
-
-    public NeoForgeMod() {
-
-    }
-
-    @SubscribeEvent
-    public void onInit(FMLCommonSetupEvent event) {
-
-    }
 
 }
