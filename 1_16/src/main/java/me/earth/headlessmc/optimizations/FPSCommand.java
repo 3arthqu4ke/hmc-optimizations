a6c4e78b24b5230e710c98bcb99f44fd3492aa01
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2024-08-25 09:51:54 +0200

    1.16.5 support

---
+++ 1_16/src/main/java/me/earth/headlessmc/optimizations/FPSCommand.java
@@ -0,0 +1,17 @@
+package me.earth.headlessmc.optimizations;
+
+import me.earth.headlessmc.api.HeadlessMc;
+import me.earth.headlessmc.api.command.AbstractCommand;
+import net.minecraft.client.Minecraft;
+
+public class FPSCommand extends AbstractCommand {
+    public FPSCommand(HeadlessMc ctx) {
+        super(ctx, "fps", "Prints out the current FPS.");
+    }
+
+    @Override
+    public void execute(String s, String... strings) {
+        ctx.log(Minecraft.getInstance().fpsString);
+    }
+
+}
