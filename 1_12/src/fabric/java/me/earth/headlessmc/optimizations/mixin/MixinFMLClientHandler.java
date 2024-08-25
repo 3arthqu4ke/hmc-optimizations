04260ad20ab8327b0072cca3e49a01690c9aff58
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2024-08-25 13:16:43 +0200

    Fixed 1.12.2, TODO: are we really using Mixin 0.7.11???

--- 1_12/src/fabric/java/me/earth/headlessmc/optimizations/mixin/MixinFMLClientHandler.java
+++ 1_12/src/fabric/java/me/earth/headlessmc/optimizations/mixin/MixinFMLClientHandler.java
@@ -4,7 +4,7 @@ import me.earth.headlessmc.optimizations.FPSCommand;
 import org.spongepowered.asm.mixin.Mixin;
 
 @SuppressWarnings({"unused", "UnusedMixin"})
-@Mixin(value = FPSCommand.class)
+@Mixin(value = FPSCommand.class, remap = false)
 public abstract class MixinFMLClientHandler {
     // dummy because FMLClientHandler does not exist on Fabric
 
